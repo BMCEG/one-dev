61dbe60bf3d873930c1e45b996d52c43eaccc59a
alimoneib <ali_moneib@yahoo.com>
2021-12-08 14:46:18 +0200

    small fix

--- onedev_backend/api/views.py
+++ onedev_backend/api/views.py
@@ -12,7 +12,7 @@ scopes = [
 'https://www.googleapis.com/auth/spreadsheets',
 'https://www.googleapis.com/auth/drive'
 ]
-credentials = ServiceAccountCredentials.from_json_keyfile_name("emails.json", scopes) #access the json key you downloaded earlier 
+credentials = ServiceAccountCredentials.from_json_keyfile_name("landingForm.json", scopes) #access the json key you downloaded earlier 
 file = gspread.authorize(credentials) # authenticate the JSON key with gspread
 sheet = file.open("OneDev Data")  #open sheet
 sheet = sheet.sheet1  #replace sheet_name with the name that corresponds to yours, e.g, it can be sheet1
